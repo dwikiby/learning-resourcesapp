a8895855c28bd6b7e4b3bc59e537ac0829cb7218
Dwiki Ramadhan <dwikibyy@gmail.com>
2022-12-07 11:59:20 +0700

    adding a base button

--- src/components/learning-resources/LearningResource.vue
+++ src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -29,5 +29,24 @@ header {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
 }
 </style>
