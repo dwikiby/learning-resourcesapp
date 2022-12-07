520fa474fd295178f1eb1d225ee3cead9d97dfe0
Dwiki Ramadhan <dwikibyy@gmail.com>
2022-12-07 08:40:30 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentasion.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn more in google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+};
+</script>
