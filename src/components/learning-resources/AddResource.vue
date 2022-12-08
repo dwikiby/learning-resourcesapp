262e89826b37c642ff98b9f39468c4814550714c
Dwiki Ramadhan <dwikibyy@gmail.com>
2022-12-08 13:46:04 +0700

    adding a modal dialog

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,21 @@
 <template>
+  <base-dialog
+    v-if="inputIsInvalid"
+    title="Invalid Input"
+    @close="confirmError"
+  >
+    <template #default>
+      <p>Unfortunately, at least one input value is invalid.</p>
+      <p>
+        Please check your input and make sure you enter at least a few
+        characters into each input field.
+      </p>
+    </template>
+    <template #actions>
+      <base-button @click="confirmError">Okay</base-button>
+    </template>
+  </base-dialog>
+
   <base-card>
     <form @submit.prevent="submitData">
       <div class="form-control">
@@ -29,7 +46,9 @@
 export default {
   inject: ['addResource'],
   data() {
-    return {};
+    return {
+      inputIsInvalid: false,
+    };
   },
   methods: {
     submitData() {
@@ -37,7 +56,19 @@ export default {
       const enteredDescription = this.$refs.descInput.value;
       const enteredUrl = this.$refs.linkInput.value;
 
+      if (
+        enteredTitle.trim() === '' ||
+        enteredDescription.trim() === '' ||
+        enteredUrl.trim() === ''
+      ) {
+        this.inputIsInvalid = true;
+        return;
+      }
+
       this.addResource(enteredTitle, enteredDescription, enteredUrl);
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
     },
   },
 };
